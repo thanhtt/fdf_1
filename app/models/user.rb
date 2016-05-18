f01b0a4abfcfa3045084e1dcb91b17c8bddc2a4f
thanhtt <tran.tien.thanh@framgia.com>
2016-05-18 14:39:57 +0700

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :suggests, dependent: :destroy
+
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
 end
