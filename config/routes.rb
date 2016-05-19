3d2d16a46c832b8a6f3e09bdc76b237f8430c847
thanhtt <tran.tien.thanh@framgia.com>
2016-05-19 11:14:41 +0700

    auth_facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "callbacks"}
   root "static_pages#home"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518084642) do
+ActiveRecord::Schema.define(version: 20160519041305) do
 
   create_table "categories", force: :cascade do |t|
     t.text     "name"
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 20160518084642) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
