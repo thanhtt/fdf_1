8e5c3fd170791df60721f5118defed8ba382d4cd
thanhtt <tran.tien.thanh@framgia.com>
2016-05-20 16:36:01 +0700

    Suggestion

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,5 +3,7 @@ Config.setup do |config|
   config.const_name = "Settings"
 
   # Ability to remove elements of the array set in earlier loaded settings file. Default: nil
-  # config.knockout_prefix = '--'
+  # config.knockout_prefix = "--"
+  ICONS = {google_oauth2: "google plus", facebook: "facebook"}
+  COLOR = {google_oauth2: "blue", facebook: "facebook"}
 end
